faaee7acc387ee7bdb2a7105f64e8c612ea41772
asamitakahashi <1105asamit@i.softbank.jp>
2020-07-18 18:02:08 +0900

    ボタン機能追加

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -28,7 +28,35 @@ class ViewController: UIViewController {
         let image = UIImage(named: "e")
         imageView.image = image
     }
-
+    //表示している画像の番号
+    var displayImageNo = 0
+    
+    //画像の名前の配列
+    let imageNameArray = ["a", "b", "c","d", "e"]
+    
+    //表示指定る画像の番号を元に画像を表示する
+    func displayImage() {
+        
+        //表示している画像の番号から名前を取り出し
+        let name = imageNameArray[displayImageNo]
+        
+        //画像を読み込み
+        let image = UIImage(named: name)
+        
+        //Image Viewに読み込んだ画像をセット
+        imageView.image = image
+        
+    }
+    //タップした場合
+    //進むボタン
+    @IBAction func nextButtonTap(_ sender: Any) {
+    }
+    //戻るボタン
+    @IBAction func backButtonTap(_ sender: Any) {
+    }
+    @IBAction func switchButtonTap(_ sender: Any) {
+    }
+    
 
 }
 
